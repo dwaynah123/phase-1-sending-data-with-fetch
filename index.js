7946af7f26f9ebf927e51f931a8cdac571bdfb9c
Dwayna Harris <dwaynaharris@gmail.com>
2023-06-02 22:02:22 -0400

    pratice using fetch() to send post request and using catch to identify errors

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    }),
+  };
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((object) => {
+        console.log((object));
+        document.querySelector("body").append(object.id);
+    })
+    .catch((message) => {
+        console.log(message.message);
+        document.querySelector("body").append(message.message);
+      });
+}
+
